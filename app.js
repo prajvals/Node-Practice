238b1ec19cb7549f0378d237bd6de47ed3d17415
Prajval Singh <prajvaltheking@gmail.com>
2022-03-25 14:56:51 +0530

    Made Post request

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf8')
 );
 
+app.use(express.json());
+
 const port = 4005;
 app.listen(port, () => {
   console.log('yeah my server is listening ');
@@ -16,6 +18,13 @@ app.listen(port, () => {
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).send({
     result: 'Success',
-    data: tours,
+    data: {
+      tours,
+    },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body); // we get an undefined value when we make a post request
+  res.send('done');
 });
