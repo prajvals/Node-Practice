3795e7d7663aaae159a5bb7b9007b1e78cfe1434
Prajval Singh <prajvaltheking@gmail.com>
2022-03-28 15:04:37 +0530

    implemented search functionality in it

--- app.js
+++ app.js
@@ -24,22 +24,33 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body); // we get an undefined value when we make a post request
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'Success',
-        data: {
-          newTour,
-        },
-      });
-    }
-  );
-  // res.send('done'); 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  req.params.id = req.params.id * 1;
+  const particularTour = tours.find((el) => req.params.id === el.id);
+  console.log(particularTour);
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour: particularTour,
+    },
+  });
+  // res.send(particularTour);
+  // console.log(req.body); // we get an undefined value when we make a post request
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     res.status(201).json({
+  //       status: 'Success',
+  //       data: {
+  //         newTour,
+  //       },
+  //     });
+  //   }
+  // );
+  // res.send('done');
 });
