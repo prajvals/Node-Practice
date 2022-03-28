3a9395362877967b72a58354a2b2cab9ebbf9c86
Prajval Singh <prajvaltheking@gmail.com>
2022-03-28 14:46:31 +0530

    setting up the first post request

--- app.js
+++ app.js
@@ -26,5 +26,20 @@ app.get('/api/v1/tours', (req, res) => {
 
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body); // we get an undefined value when we make a post request
-  res.send('done');
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          newTour,
+        },
+      });
+    }
+  );
+  // res.send('done'); 
 });
