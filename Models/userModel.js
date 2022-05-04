87f1d0f92fcb286a13df8ed657b89147f40d9a05
Prajval Singh <prajvaltheking@gmail.com>
2022-05-04 21:50:26 +0530

    encrypted password

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 //name,email,photo,password,passwordConfirm
 const userSchema = new mongoose.Schema({
   name: {
@@ -27,7 +28,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 8,
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
