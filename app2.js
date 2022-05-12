292a599349d28cfcb4ce9b8760083563f66d4224
Prajval Singh <prajvaltheking@gmail.com>
2022-05-12 09:19:45 +0530

    security settings implemented, rate-limiting, security headers, and req.body size limiting

--- app2.js
+++ app2.js
@@ -1,3 +1,5 @@
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const mongoose = require('mongoose');
 const { json } = require('express/lib/response');
@@ -9,13 +11,26 @@ const userRouter = require('./Routes/UserRouter');
 const globalErrorObject = require('./Utils/AppError');
 const globalErrorHandler = require('./Controllers/AppErrorController');
 
-console.clear();
-// process.env.NODE_ENV == 'production';
-console.log(process.env.NODE_ENV);
+console.clear(); //only for clearing terminal in nodemon
+
+//setting security headers
+app.use(helmet());
+
+//rateLimiting
+const limitRate = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too Many requests, please try again after one hour',
+});
+app.use('/api', limitRate);
+
+//development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+app.use(express.json({ limit: '10kb' }));
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
@@ -40,3 +55,12 @@ app.all('*', (req, res, next) => {
 app.use(globalErrorHandler);
 
 module.exports = app;
+
+
+/*
+see helmet is for setting security headers which browsers understand and will incorporate on it alright yeah 
+ratelimiting is to limit the number of requests that a particular ip address can make in a particular time span
+ratelimiting is applied on a particular route and it keeps tracks of all the requests coming to that route
+also setting the limit in json parser is where we can specify till what size are body allowed in here
+
+*/
