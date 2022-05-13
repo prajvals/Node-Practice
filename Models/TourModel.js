2e1bbca73f169f2af3d8228f26427bc787494d19
Prajval Singh <prajvaltheking@gmail.com>
2022-05-13 16:50:43 +0530

    embedded users inside tours

--- Models/TourModel.js
+++ Models/TourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 //you can send any other data apart from what is defined in the schema
 //but only what is defined in the schema would be the one taken up
@@ -99,6 +100,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     //note we have to enable the virtual in the toJSON, these are the schema options
@@ -125,6 +127,26 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true }); //slug is the last part of the url usually used to identify the resource from it alright yeah
   next();
 });
+
+//this is for the embedding the user inside the tour model
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => {
+    await User.findById(id);
+  });
+  this.guides = await Promise.all(guidesPromises);
+});
+//very important note here
+/*
+see when we use async await, we are actually awaiting the response alright
+that response is something we can use later on inside the function alright yeah 
+it basically awaits the execution flow at that and you can use that value inside it alright yeah
+but if you are to use that value outside of the function, remember that an async function will always return a promise alright yeah 
+and even if the return statement has the await of some promise
+the main function in which its defined would always return a promise only 
+okay
+*/
+ 
+
 
 //post is called when the data has been successfully saved in the database
 tourSchema.post('save', function (doc, next) {
