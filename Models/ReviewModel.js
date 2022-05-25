408da01ca4e00d8d284fb0fbbf66ea6b9abac04d
Prajval Singh <prajvaltheking@gmail.com>
2022-05-25 15:12:52 +0530

    added populate for the reviewModel

--- Models/ReviewModel.js
+++ Models/ReviewModel.js
@@ -1,30 +1,53 @@
 const mongoose = require('mongoose');
 
 //review,rating,createdAt,ref to tour,ref to user
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'Review cannot be empty'],
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      min: [1, 'A rating cannot be less than 1'],
+      max: [5, 'A rating cannot have more than 5'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'tours',
+      required: [true, 'A review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A user must belong to a tour'],
+    },
   },
-  rating: {
-    type: Number,
-    min: [1, 'A rating cannot be less than 1'],
-    max: [5, 'A rating cannot have more than 5'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  tour: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Tour',
-    required: [true, 'A review must belong to a tour'],
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'A user must belong to a tour'],
-  },
+  {
+    //note we have to enable the virtual in the toJSON, these are the schema options
+    toJSON: {
+      virtuals: false,
+    },
+    toObject: {
+      virtuals: false,
+    },
+    bufferCommands: false,
+  }
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  }).populate({
+    path: 'tour',
+    select: 'name',
+  });
+  next();
 });
 
 module.exports = mongoose.model('Reviews', reviewSchema);
